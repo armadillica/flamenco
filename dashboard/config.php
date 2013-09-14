4b7ab71eca4b07ad3bf1c79dec43897045d0e2fb
fsiddi <francesco.siddi@gmail.com>
2013-09-15 01:37:58 +0200

    Refactoring: removed unused dashboard PHP code

--- dashboard/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$debug = False;
-/* Get the port for the service. */
-$service_port = 6000;
-/* Get the IP address for the target host. */
-$address = 'localhost';
-?>
--- dashboard/pages/404.php
+++
@@ -1,3 +0,0 @@
-	<div class="page-header">
-	    <h1>404 - Page not found</h1>
-	</div>
