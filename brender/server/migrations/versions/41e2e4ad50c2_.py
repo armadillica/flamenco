32832c414356a842780866c933afded258f18ce0
Matthieu Simon <matthieu.simon@qarnot-computing.com>
2015-01-13 17:59:44 +0100

    upgrade database (add relation_job_manager table)

---
+++ brender/server/migrations/versions/41e2e4ad50c2_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 41e2e4ad50c2
+Revises: f505650d7b3
+Create Date: 2015-01-09 16:09:42.185604
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '41e2e4ad50c2'
+down_revision = 'f505650d7b3'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
