a6c6c086d651ad06602247aebb621a255df58b1c
Francesco Siddi <francesco.siddi@gmail.com>
2015-01-19 00:51:35 +0100

    Improved default config for dashboard

--- brender/dashboard/application/__init__.py
+++ brender/dashboard/application/__init__.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 
 app.config.update(
     SECRET_KEY='A0Zr98j/3yX R~XHH!jmN]LWX/,?RT',
-    DEBUG=False,
-    HOST='localhost',
-    PORT=8888,
-    BRENDER_SERVER='localhost:9999'
 )
 
+try:
+    from application import config
+    app.config['BRENDER_SERVER'] = config.Config.BRENDER_SERVER
+except ImportError:
+    app.config['BRENDER_SERVER'] = 'localhost:9999'
 
 def check_connection():
     try:
@@ -102,14 +103,3 @@ def page_not_found(error):
 def server_error(error):
     return render_template('500_error.html', error=error.to_html()), 500
 
-def run(user_config=None):
-    config = app.config
-
-    if user_config:
-        config.from_object(user_config)
-
-    config.update(
-        SERVER_NAME="%s:%s" % (config['HOST'], config['PORT'])
-    )
-
-    app.run(config['HOST'], config['PORT'])
