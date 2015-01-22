cfbf9c37f7f7beec5989fc9c80f9dcc372fd31cd
Eibriel <eibriel@yahoo.com.ar>
2015-01-22 17:23:26 +0100

    Fixed some Migration problems with Add Constraint in Manager Table version (deleting it)

--- brender/server/migrations/versions/4e25f581d8bd_virtual_workers.py
+++ brender/server/migrations/versions/4e25f581d8bd_virtual_workers.py
@@ -1,14 +1,14 @@
 """virtual workers
 
 Revision ID: 4e25f581d8bd
-Revises: 25f0840c5d87
+Revises: 593c59c4c834
 Create Date: 2015-01-20 16:35:26.855490
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '4e25f581d8bd'
-down_revision = '25f0840c5d87'
+down_revision = '593c59c4c834'
 
 from alembic import op
 import sqlalchemy as sa
