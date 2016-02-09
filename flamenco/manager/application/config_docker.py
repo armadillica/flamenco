7a1b7758842c435af8746804e196af782f7fe04e
Francesco Siddi <francesco.siddi@gmail.com>
2016-02-09 11:33:37 +0100

    Renamed DATABASE_URI to work with readme
    In the readme we instruct to link against a mysql container called
    mysql, not mysql_flamenco.

--- flamenco/manager/application/config_docker.py
+++ flamenco/manager/application/config_docker.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     FLAMENCO_SERVER = 'flamenco_server:9999'
-    DATABASE_URI = 'mysql://root:root@mysql_flamenco'
+    DATABASE_URI = 'mysql://root:root@mysql'
     DATABASE_NAME = 'manager'
     SQLALCHEMY_DATABASE_URI = os.path.join(DATABASE_URI, DATABASE_NAME)
     MANAGER_STORAGE = '/data/storage/manager'
