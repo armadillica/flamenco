9295e1082b8fe006d4d52419d2335b4cdf3414b3
Francesco Siddi <francesco.siddi@gmail.com>
2016-07-17 18:07:03 +0200

    Comments to settings.py

--- flamenco/server-eve/application/modules/flamenco/settings.py
+++ flamenco/server-eve/application/modules/flamenco/settings.py
@@ -20,10 +20,13 @@ jobs_schema = {
         'type': 'string',
         'required': True,
     },
+    # Defines how we are going to parse the settings field, in order to generate
+    # the tasks list.
     'job_type': {
         'type': 'string',
         'required': True,
     },
+    # Remarks about the settings, the author or the system
     'notes': {
         'type': 'string',
     },
@@ -44,6 +47,9 @@ jobs_schema = {
             'embeddable': True
         },
     },
+    # We currently say that a job, and all its tasks, will be assigned to one
+    # manager only. If one day we want to allow multiple managers to handle a
+    # job we can convert this to a list.
     'manager': {
         'type': 'objectid',
         'data_relation': {
@@ -62,12 +68,15 @@ jobs_schema = {
             'failed'],
         'default': 'queued'
     },
+    # This number could be also be a float between 0 and 1.
     'priority': {
         'type': 'integer',
         'min': 1,
         'max': 100,
         'default': 50
     },
+    # Embedded summary of the status of all tasks of a job. Used when listing
+    # all jobs via a graphical interface.
     'tasks_status': {
         'type': 'dict',
         'schema': {
@@ -77,6 +86,8 @@ jobs_schema = {
             'canceled': {'type': 'integer'}
         }
     },
+    # The most important part of a job. These custom values are parsed by the
+    # job compiler in order to generate the tasks.
     'settings': {
         'type': 'dict',
         # TODO: introduce dynamic validator, based on job_type/task_type
@@ -142,12 +153,15 @@ tasks_schema = {
         'type': 'string',
         'maxlength': 128
     },
-    'child': {
-        'type': 'objectid',
-        'data_relation': {
-            'resource': 'tasks',
-            'field': '_id',
-            'embeddable': True
+    'parents': {
+        'type': 'list',
+        'schema': {
+            'type': 'objectid',
+            'data_relation': {
+                'resource': 'tasks',
+                'field': '_id',
+                'embeddable': True
+            }
         },
     },
     'worker': {
