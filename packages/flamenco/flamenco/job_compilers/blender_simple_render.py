be48a23cdb7b18cb7cb5b732bcd94905e92bc9d5
Francesco Siddi <francesco.siddi@gmail.com>
2016-11-09 23:57:23 +0100

    Basic job compilers

---
+++ packages/flamenco/flamenco/job_compilers/blender_simple_render.py
@@ -0,0 +1,57 @@
+from application.modules.flamenco.utils import frame_range_parse
+from application.modules.flamenco.utils import frame_range_merge
+
+
+class JobCompiler:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def compile(job, create_task):
+        """The basic Blender render job."""
+        job_settings = job['settings']
+        parsed_frames = frame_range_parse(job_settings['frames'])
+        chunk_size = job_settings['chunk_size']
+        try:
+            render_output = job_settings['render_output']
+        except KeyError:
+            render_output = None
+        for i in range(0, len(parsed_frames), chunk_size):
+            commands = []
+
+            if not job_settings['filepath'].startswith('/'):
+                cmd_download = {
+                    'name': 'download',
+                    'settings': {}
+                }
+                commands.append(cmd_download)
+
+                cmd_unzip = {
+                    'name': 'unzip',
+                    'settings': {}
+                }
+                commands.append(cmd_unzip)
+
+            frames = frame_range_merge(parsed_frames[i:i + chunk_size])
+            cmd_render = {
+                'name': 'blender_render',
+                'settings': {
+                    'filepath': job_settings['filepath'],
+                    'format': job_settings['format'],
+                    'frames': frames
+                }
+            }
+            if render_output:
+                cmd_render['settings']['render_output'] = render_output
+
+            commands.append(cmd_render)
+
+            if not render_output:
+                cmd_upload = {
+                    'name': 'upload',
+                    'settings': {}
+                }
+                commands.append(cmd_upload)
+
+            create_task(job, commands, frames)
