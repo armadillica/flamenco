82a8f72dba40748fb23143b66e16b0b25343b7ea
Francesco Siddi <francesco.siddi@gmail.com>
2016-12-01 00:34:05 +0100

    Take advantage of subcommands

--- packages/flamenco/flamenco/cli.py
+++ packages/flamenco/flamenco/cli.py
@@ -3,6 +3,7 @@
 import logging
 
 from flask import current_app
+from flask_script import Manager
 
 from pillar.cli import manager, create_service_account
 from pillar.api.utils import authentication
@@ -11,11 +12,13 @@ import flamenco.setup
 
 log = logging.getLogger(__name__)
 
+manager_flamenco = Manager(current_app, usage="Perform Flamenco operations")
 
-@manager.command
-@manager.option('-r', '--replace', dest='replace', action='store_true',
-                default=False)
-@manager.option('-s', '--svn', dest='svn_url', nargs='?')
+
+@manager_flamenco.command
+@manager_flamenco.option('-r', '--replace', dest='replace', action='store_true',
+                         default=False)
+@manager_flamenco.option('-s', '--svn', dest='svn_url', nargs='?')
 def setup_for_flamenco(project_url, replace=False, svn_url=None):
     """Adds Flamenco node types to the project.
 
@@ -26,3 +29,5 @@ def setup_for_flamenco(project_url, replace=False, svn_url=None):
     authentication.force_cli_user()
     flamenco.setup.setup_for_flamenco(project_url, replace=replace,
                                       svn_url=svn_url)
+
+manager.add_command("flamenco", manager_flamenco)
