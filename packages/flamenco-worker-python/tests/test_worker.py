817added1c735c46fde75a24c0168c377ed37f27
Sybren A. Stüvel <sybren@stuvel.eu>
2017-01-03 15:33:33 +0100

    Worker: moved mocked responses into their own python file.

--- packages/flamenco-worker-python/tests/test_worker.py
+++ packages/flamenco-worker-python/tests/test_worker.py
@@ -4,42 +4,6 @@ from unittest.mock import Mock
 
 import asyncio
 import requests
-import attr
-
-
-@attr.s
-class JsonResponse:
-    """Mocked HTTP response returning JSON.
-
-    Maybe we want to switch to using unittest.mock.Mock for this,
-    or to using the responses package.
-    """
-
-    _json = attr.ib()
-    status_code = attr.ib(default=200, validator=attr.validators.instance_of(int))
-
-    def json(self):
-        return self._json
-
-    def raise_for_status(self):
-        if 200 <= self.status_code < 300:
-            return
-
-        raise requests.HTTPError(self.status_code)
-
-
-@attr.s
-class EmptyResponse:
-    """Mocked HTTP response returning an empty 204.
-
-    Maybe we want to switch to using unittest.mock.Mock for this,
-    or to using the responses package.
-    """
-
-    status_code = attr.ib(default=204, validator=attr.validators.instance_of(int))
-
-    def raise_for_status(self):
-        pass
 
 
 class AbstractWorkerTest(unittest.TestCase):
@@ -83,6 +47,7 @@ class WorkerStartupTest(AbstractWorkerTest):
 
     def test_startup_registration(self):
         from flamenco_worker.worker import detect_platform
+        from mock_responses import JsonResponse
 
         self.worker.worker_id = None
 
@@ -109,6 +74,7 @@ class WorkerStartupTest(AbstractWorkerTest):
         """Test that startup is aborted when the worker can't register."""
 
         from flamenco_worker.worker import detect_platform
+        from mock_responses import JsonResponse
 
         self.worker.worker_id = None
 
@@ -149,6 +115,7 @@ class TestWorkerTaskFetch(AbstractWorkerTest):
 
     def test_fetch_task_happy(self):
         from unittest.mock import call
+        from mock_responses import JsonResponse, EmptyResponse
 
         self.manager.post = Mock()
         self.manager.post.side_effect = [
