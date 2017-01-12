de929809abcc5388d093a16b4fd916eead305b5a
Sybren A. Stüvel <sybren@stuvel.eu>
2017-01-12 13:42:42 +0100

    Server: removed unused code & fixed indentation

--- packages/flamenco/flamenco/jobs/__init__.py
+++ packages/flamenco/flamenco/jobs/__init__.py
@@ -91,62 +91,6 @@ class JobManager(object):
         job.update(r)
         return job
 
-    def edit_job(self, job_id, **fields):
-        """Edits a job.
-
-        :type job_id: str
-        :type fields: dict
-        :rtype: pillarsdk.Node
-        """
-
-        api = pillar_api()
-        job = pillarsdk.Node.find(job_id, api=api)
-
-        job._etag = fields.pop('_etag')
-        job.name = fields.pop('name')
-        job.description = fields.pop('description')
-        job.status = fields.pop('status')
-        job.properties.job_type = fields.pop('job_type', '').strip() or None
-
-        users = fields.pop('users', None)
-        job.properties.assigned_to = {'users': users or []}
-
-        self._log.info('Saving job %s', job.to_dict())
-
-        if fields:
-            self._log.warning('edit_job(%r, ...) called with unknown fields %r; ignoring them.',
-                              job_id, fields)
-
-        job.update(api=api)
-        return job
-
-    def delete_job(self, job_id, etag):
-        api = pillar_api()
-
-        self._log.info('Deleting job %s', job_id)
-        job = pillarsdk.Resource({'_id': job_id, '_etag': etag})
-        job.path = 'flamenco/jobs'
-        job.delete(api=api)
-
-    def jobs_for_user(self, user_id):
-        """Returns the jobs for the given user.
-
-        :returns: {'_items': [job, job, ...], '_meta': {Eve metadata}}
-        """
-
-        api = pillar_api()
-
-        # TODO: also include jobs assigned to any of the user's groups.
-        jobs = pillarsdk.resource.List()
-        jobs.list_class.path = 'flamenco/jobs'
-        j = jobs.all({
-            'where': {
-                'user': user_id,
-            }
-        }, api=api)
-
-        return j
-
     def jobs_for_project(self, project_id):
         """Returns the jobs for the given project.
 
@@ -165,8 +109,8 @@ class JobManager(object):
     def job_status_summary(self, project_id):
         """Returns number of shots per shot status for the given project.
 
-                :rtype: ProjectSummary
-                """
+        :rtype: ProjectSummary
+        """
 
         api = pillar_api()
 
