a8c04fc52354b4192fd6712f55872c87c1895b86
Sybren A. Stüvel <sybren@stuvel.eu>
2017-01-12 16:38:03 +0100

    Removed old Flamenco Manager

--- packages/flamenco-manager/flamenco_manager/application/config_docker.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config(object):
-    FLAMENCO_SERVER = 'flamenco_server:9999'
-    DATABASE_URI = 'mysql://root:root@mysql'
-    DATABASE_NAME = 'manager'
-    SQLALCHEMY_DATABASE_URI = os.path.join(DATABASE_URI, DATABASE_NAME)
-    MANAGER_STORAGE = '/data/storage/manager'
