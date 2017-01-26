bde4d6dd74a3bd75e40f34b9688cbece584f143b
Sybren A. Stüvel <sybren@stuvel.eu>
2017-01-26 18:05:35 +0100

    Bumped version to 2.0-beta7

--- packages/flamenco-worker-python/setup.py
+++ packages/flamenco-worker-python/setup.py
@@ -5,7 +5,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='flamenco-worker',
-        version='2.0-beta6',
+        version='2.0-beta7',
         description='Flamenco Worker implementation',
         author='Sybren A. Stüvel',
         author_email='sybren@blender.studio',
