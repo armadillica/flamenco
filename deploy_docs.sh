1c74d3d3c58aa71c7d9ebf93bd42ed869da686d8
Francesco Siddi <francesco.siddi@gmail.com>
2017-02-05 18:06:58 +0100

    Docs: deploy script

---
+++ deploy_docs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd docs
+command -v greadlink 2>/dev/null 2>&1 || { echo >&2 "Command mkdocs not found. Are you in the right venv?"; exit 1; }
+mkdocs build
+rsync -auv ./site/* armadillica@flamenco.io:/home/armadillica/flamenco.io/docs
