50bde25b55824c1594821d8a1d3052c5b9432cca
Sybren A. Stüvel <sybren@stuvel.eu>
2017-03-03 16:32:40 +0100

    Set up requirements to link to Pillar and the SDK

--- setup.py
+++ setup.py
@@ -8,9 +8,7 @@ setuptools.setup(
     name='flamenco',
     version='2.0-beta10',
     packages=setuptools.find_packages('.', exclude=['test']),
-    install_requires=[
-        'pillar>=2.0',
-    ],
+    install_requires=[],
     tests_require=[
         'pytest>=2.9.1',
         'responses>=0.5.1',
