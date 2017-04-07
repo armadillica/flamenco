8a47f8e75f6cff9bdbe135d5ac237dbe78d4d110
Sybren A. Stüvel <sybren@stuvel.eu>
2017-04-07 12:23:36 +0200

    Added example commit & tag cmds to update_version.sh

--- update_version.sh
+++ update_version.sh
@@ -9,4 +9,6 @@ sed "s/version='[^']*'/version='$1'/" -i setup.py
 
 git diff
 echo
-echo "Don't forget to tag and commit!"
+echo "Don't forget to commit and tag:"
+echo git commit -m \'Bumped version to $1\' setup.py
+echo git tag -a v$1 -m \'Tagged version $1\'
