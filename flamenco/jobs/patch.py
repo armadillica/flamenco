2f7591893974da4627e6f7d18500b46f44b1b790
Sybren A. Stüvel <sybren@stuvel.eu>
2017-05-05 17:56:29 +0200

    Using Pillar's AbstractPatchHandler for job and task patching.

--- flamenco/jobs/patch.py
+++ flamenco/jobs/patch.py
@@ -1,80 +1,38 @@
-"""Job patching support.
-
-TODO Sybren: merge identical code in jobs/patch.py and tasks/patch.py into
-something more generic/reusable.
-"""
+"""Job patching support."""
 
 import logging
 
+import bson
+from flask import Blueprint
 import werkzeug.exceptions as wz_exceptions
-from flask import Blueprint, request
-from pillar.api.utils import authorization, authentication, str2id
+
+from pillar.api.utils import authorization
+from pillar.api import patch_handler
 
 log = logging.getLogger(__name__)
 patch_api_blueprint = Blueprint('flamenco.jobs.patch', __name__)
 
-patch_handlers = {}
 
+class JobPatchHandler(patch_handler.AbstractPatchHandler):
+    item_name = 'job'
 
-def patch_handler(operation):
-    """Decorator, marks the decorated function as PATCH handler."""
+    @authorization.require_login(require_roles={'flamenco-admin'})
+    def patch_set_job_status(self, job_id: bson.ObjectId, patch: dict):
+        """Updates a job's status in the database."""
 
-    def decorator(func):
-        patch_handlers[operation] = func
-        return func
+        from flamenco import current_flamenco
+        from pillar.api.utils.authentication import current_user_id
 
-    return decorator
+        new_status = patch['status']
 
-
-@patch_api_blueprint.route('/<job_id>', methods=['PATCH'])
-@authorization.require_login()
-def patch_job(job_id):
-    # Parse the request
-    job_id = str2id(job_id)
-    patch = request.get_json()
-    if not patch:
-        raise wz_exceptions.BadRequest('Patch must contain JSON')
-
-    try:
-        patch_op = patch['op']
-    except KeyError:
-        raise wz_exceptions.BadRequest("PATCH should contain 'op' key to denote operation.")
-
-    log.debug('User %s wants to PATCH "%s" job %s',
-              authentication.current_user_id(), patch_op, job_id)
-
-    # Find the PATCH handler for the operation.
-    try:
-        handler = patch_handlers[patch_op]
-    except KeyError:
-        log.warning('No PATCH handler for operation %r', patch_op)
-        raise wz_exceptions.BadRequest('Operation %r not supported' % patch_op)
-
-    # Let the PATCH handler do its thing.
-    return handler(job_id, patch)
-
-
-@patch_handler('set-job-status')
-@authorization.require_login(require_roles={'flamenco-admin'})
-def patch_set_job_status(job_id, patch):
-    """Updates a job's status in the database."""
-
-    from flamenco import current_flamenco
-    from pillar.api.utils import str2id
-    from pillar.api.utils.authentication import current_user_id
-
-    new_status = patch['status']
-    job_id = str2id(job_id)
-
-    log.info('User %s uses PATCH to set job %s status to "%s"',
-             current_user_id(), job_id, new_status)
-    try:
-        current_flamenco.job_manager.api_set_job_status(job_id, new_status)
-    except ValueError:
-        raise wz_exceptions.UnprocessableEntity('Status %s is invalid' % new_status)
-
-    return '', 204
+        log.info('User %s uses PATCH to set job %s status to "%s"',
+                 current_user_id(), job_id, new_status)
+        try:
+            current_flamenco.job_manager.api_set_job_status(job_id, new_status)
+        except ValueError:
+            raise wz_exceptions.UnprocessableEntity(f'Status {new_status} is invalid')
 
 
 def setup_app(app):
+    JobPatchHandler(patch_api_blueprint)
     app.register_api_blueprint(patch_api_blueprint, url_prefix='/flamenco/jobs')
--- flamenco/tasks/patch.py
+++ flamenco/tasks/patch.py
@@ -2,82 +2,44 @@
 
 import logging
 
+import bson
+from flask import Blueprint
 import werkzeug.exceptions as wz_exceptions
-from flask import Blueprint, request
-from pillar.api.utils import authorization, authentication, str2id
 
-from flamenco import current_flamenco
+from pillar.api.utils import authorization
+from pillar.api import patch_handler
 
 log = logging.getLogger(__name__)
 blueprint = Blueprint('flamenco.tasks.patch', __name__)
 
-patch_handlers = {}
 
+class TaskPatchHandler(patch_handler.AbstractPatchHandler):
+    item_name = 'task'
 
-def patch_handler(operation):
-    """Decorator, marks the decorated function as PATCH handler."""
+    @authorization.require_login(require_roles={'flamenco-admin'})
+    def patch_set_task_status(self, task_id: bson.ObjectId, patch: dict):
+        """Updates a task's status in the database."""
 
-    def decorator(func):
-        patch_handlers[operation] = func
-        return func
+        from flamenco import current_flamenco
 
-    return decorator
+        new_status = patch['status']
 
+        # FIXME Sybren: add permission check.
 
-@blueprint.route('/<task_id>', methods=['PATCH'])
-@authorization.require_login()
-def patch_task(task_id):
-    # Parse the request
-    task_id = str2id(task_id)
-    patch = request.get_json()
+        try:
+            current_flamenco.update_status('tasks', task_id, new_status)
+        except ValueError:
+            raise wz_exceptions.UnprocessableEntity('Invalid status')
 
-    try:
-        patch_op = patch['op']
-    except KeyError:
-        raise wz_exceptions.BadRequest("PATCH should contain 'op' key to denote operation.")
+        # also inspect other tasks of the same job, and possibly update the job status as well.
+        tasks_coll = current_flamenco.db('tasks')
+        task_job = tasks_coll.find_one({'_id': task_id}, projection={'job': 1})
 
-    log.debug('User %s wants to PATCH %s task %s',
-              authentication.current_user_id(), patch_op, task_id)
-
-    # Find the PATCH handler for the operation.
-    try:
-        handler = patch_handlers[patch_op]
-    except KeyError:
-        log.warning('No PATCH handler for operation %r', patch_op)
-        raise wz_exceptions.BadRequest('Operation %r not supported' % patch_op)
-
-    # Let the PATCH handler do its thing.
-    return handler(task_id, patch)
-
-
-@patch_handler('set-task-status')
-@authorization.require_login(require_roles={'flamenco-admin'})
-def patch_set_task_status(task_id, patch):
-    """Updates a task's status in the database."""
-
-    from flamenco import current_flamenco
-    from pillar.api.utils import str2id
-
-    new_status = patch['status']
-    task_id = str2id(task_id)
-
-    # FIXME Sybren: add permission check.
-
-    try:
-        current_flamenco.update_status('tasks', task_id, new_status)
-    except ValueError:
-        raise wz_exceptions.UnprocessableEntity('Invalid status')
-
-    # also inspect other tasks of the same job, and possibly update the job status as well.
-    tasks_coll = current_flamenco.db('tasks')
-    task_job = tasks_coll.find_one({'_id': task_id}, projection={'job': 1})
-
-    current_flamenco.job_manager.update_job_after_task_status_change(task_job['job'],
-                                                                     task_id,
-                                                                     new_status)
-
-    return '', 204
+        current_flamenco.job_manager.update_job_after_task_status_change(task_job['job'],
+                                                                         task_id,
+                                                                         new_status)
 
 
 def setup_app(app):
+    TaskPatchHandler(blueprint)
     app.register_api_blueprint(blueprint, url_prefix='/flamenco/tasks')
