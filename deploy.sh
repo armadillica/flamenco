1b13d22ed9d16758e4ea05627e2e093517764c50
Sybren A. Stüvel <sybren@stuvel.eu>
2018-02-06 10:49:52 +0100

    Removed deploy.sh and rsync_ui.sh
    They are no longer needed since Blender Cloud changed deployment model.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo
-echo "==========================================================================="
-echo "Dummy deploy script for people with a 'git pp' alias to push to production."
-echo "Run deploy script on your server project."
-echo "When done, press [ENTER] to stop this script."
-read dummy
