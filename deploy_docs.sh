a3ceba180ba7087d74087f1d640a103649519474
Sybren A. Stüvel <sybren@stuvel.eu>
2018-11-28 09:56:49 +0100

    Use `pipenv` in `deploy_docs.sh`

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
 cd docs
-command -v mkdocs 2>/dev/null 2>&1 || { echo >&2 "Command mkdocs not found. Are you in the right venv?"; exit 1; }
-mkdocs build
+pipenv run mkdocs build
 rsync -auv ./site/* armadillica@flamenco.io:/home/armadillica/flamenco.io/docs
