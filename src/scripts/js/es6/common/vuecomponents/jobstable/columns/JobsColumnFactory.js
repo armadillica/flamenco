3feb476ec2fb3b7d9e4974de0d7dc5bbcd849a86
Tobias Johansson <tobias@tobias-johansson.se>
2019-03-28 12:48:15 +0100

    Add Created and Updated column

--- src/scripts/js/es6/common/vuecomponents/jobstable/columns/JobsColumnFactory.js
+++ src/scripts/js/es6/common/vuecomponents/jobstable/columns/JobsColumnFactory.js
@@ -4,6 +4,8 @@ import { Status } from "./Status";
 import { Manager } from "./Manager";
 
 let ColumnFactoryBase = pillar.vuecomponents.table.columns.ColumnFactoryBase;
+let Created = pillar.vuecomponents.table.columns.Created;
+let Updated = pillar.vuecomponents.table.columns.Updated;
 
 
 class JobsColumnFactory extends ColumnFactoryBase{
@@ -17,6 +19,8 @@ class JobsColumnFactory extends ColumnFactoryBase{
             new RowObject(),
             new Manager(),
             new Priority(),
+            new Created(),
+            new Updated(),
         ]);
     }
 }
